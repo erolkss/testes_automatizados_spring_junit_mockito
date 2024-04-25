8609bd76e1b34b8ee96784c785967d58401dbead
Lucas Lima <lucaserolima@gmail.com>
2024-04-25 16:56:37 -0400

    Testing the error with invalid data when creating the planet.

--- src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetServiceTest.java
@@ -1,7 +1,9 @@
 package br.com.ero.tests.swplanetapi.domain;
 
 import static br.com.ero.tests.swplanetapi.common.PlanetConstants.PLANET;
+import static br.com.ero.tests.swplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -9,23 +11,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-//@SpringBootTest(classes = PlanetService.class)
 @ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
-  // @Autowired
   @InjectMocks
   private PlanetService planetService;
 
-  // @MockBean
   @Mock
   private PlanetRepository planetRepository;
 
-  // operacao_estado_retorno
   @Test
   public void createPlanet_WithValidData_ReturnsPlanet() {
 
@@ -35,5 +30,13 @@ public class PlanetServiceTest {
 
     assertThat(sut).isEqualTo(PLANET);
   }
-  
+
+  @Test
+  public void createPlanet_WithInvalidData_ThrowsException(){
+    when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+    assertThatThrownBy( () -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+
+
+  }
 }
