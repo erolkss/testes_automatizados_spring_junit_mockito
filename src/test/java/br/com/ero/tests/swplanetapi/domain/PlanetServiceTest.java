1d4ab997a04cb7a5fde045a6581ef903030f089d
Lucas Lima <lucaserolima@gmail.com>
2024-04-25 16:45:07 -0400

    Tests using Mockito for pure unit testing, that is, it will not be necessary to do dependency injections in the test class.

--- src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetServiceTest.java
@@ -2,20 +2,35 @@ package br.com.ero.tests.swplanetapi.domain;
 
 import static br.com.ero.tests.swplanetapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = PlanetService.class)
+//@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
-  @Autowired
+  // @Autowired
+  @InjectMocks
   private PlanetService planetService;
+
+  // @MockBean
+  @Mock
+  private PlanetRepository planetRepository;
 
   // operacao_estado_retorno
   @Test
   public void createPlanet_WithValidData_ReturnsPlanet() {
+
+    when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
     Planet sut = planetService.create(PLANET);
 
     assertThat(sut).isEqualTo(PLANET);
