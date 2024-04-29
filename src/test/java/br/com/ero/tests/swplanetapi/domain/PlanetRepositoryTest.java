6487c03f032366bda01bed49d0fb11537858e9dc
Lucas Lima <lucaserolima@gmail.com>
2024-04-29 15:58:51 -0400

    feat: Testing the success scenario between the repository and the Database

---
+++ src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,31 @@
+package br.com.ero.tests.swplanetapi.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static br.com.ero.tests.swplanetapi.common.PlanetConstants.PLANET;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+}
