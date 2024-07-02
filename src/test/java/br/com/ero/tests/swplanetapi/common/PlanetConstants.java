d885b4aa9d2b152ccf2ead7ce6c1cc1b7be7642b
Lucas Lima <lucaserolima@gmail.com>
2024-07-01 21:01:27 -0400

    feat-test: Using Mockito for Unit Testing 'createPlanet_withValidData_ReturnsPlanet'

---
+++ src/test/java/br/com/ero/tests/swplanetapi/common/PlanetConstants.java
@@ -0,0 +1,7 @@
+package br.com.ero.tests.swplanetapi.common;
+
+import br.com.ero.tests.swplanetapi.domain.Planet;
+
+public class PlanetConstants {
+    public static final Planet PLANET = new Planet("name", "climate", "terrain");
+}
---
+++ src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetServiceTest.java
@@ -0,0 +1,34 @@
+package br.com.ero.tests.swplanetapi.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static br.com.ero.tests.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class PlanetServiceTest {
+
+    @InjectMocks
+    private PlanetService planetService;
+
+    @Mock
+    private PlanetRepository planetRepository;
+
+    @Test
+    public void createPlanet_withValidData_ReturnsPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
+        Planet sut = planetService.create(PLANET);
+
+        assertThat(sut).isEqualTo(PLANET);
+    }
+}
