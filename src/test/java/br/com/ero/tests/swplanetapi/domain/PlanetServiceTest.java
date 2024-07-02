f647763745e6c0bcb5adbc6ad441b12d0bf9009d
Lucas Lima <lucaserolima@gmail.com>
2024-07-01 21:58:02 -0400

    feat-test: Created Unit Testing 'getPlanet_ByExistingId_ReturnsPlanet' and 'getPlanet_ByNonExistingId_ReturnsEmpty'

--- src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/br/com/ero/tests/swplanetapi/domain/PlanetServiceTest.java
@@ -6,10 +6,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static br.com.ero.tests.swplanetapi.common.PlanetConstants.PLANET;
 import static br.com.ero.tests.swplanetapi.common.PlanetConstants.INVALID_PLANET;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 
@@ -36,5 +37,24 @@ public class PlanetServiceTest {
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
 
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+        when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByNonExistingId_ReturnsEmpty() {
+        when(planetRepository.findById(2L)).thenReturn(Optional.empty());
+
+        Optional<Planet> sut = planetService.get(2L);
+
+        assertThat(sut).isEmpty();
     }
 }
