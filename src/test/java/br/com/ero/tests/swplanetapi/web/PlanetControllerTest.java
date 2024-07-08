f0a7cdb4ec8712cfb2b624e3b5f3244190001a36
Lucas Lima <lucaserolima@gmail.com>
2024-07-08 15:34:37 -0400

    feat-test: Integration tests with 'PlanetController', created the 'createPlanet_WithValidData_ReturnsCreated' test

---
+++ src/test/java/br/com/ero/tests/swplanetapi/web/PlanetControllerTest.java
@@ -0,0 +1,42 @@
+package br.com.ero.tests.swplanetapi.web;
+
+import br.com.ero.tests.swplanetapi.domain.PlanetService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static br.com.ero.tests.swplanetapi.common.PlanetConstants.PLANET;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.Mockito.*;
+
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets")
+                        .content(objectMapper.writeValueAsString(PLANET))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+}
