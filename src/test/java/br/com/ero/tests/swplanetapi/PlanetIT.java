a8b180e258c665bd5a91d906fcc431ee2d728fb1
Lucas Lima <lucaserolima@gmail.com>
2024-07-11 14:14:14 -0400

    Config server application for test

---
+++ src/test/java/br/com/ero/tests/swplanetapi/PlanetIT.java
@@ -0,0 +1,12 @@
+package br.com.ero.tests.swplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PlanetIT {
+
+  @Test
+  public void contextLoads() {
+  }
+}
