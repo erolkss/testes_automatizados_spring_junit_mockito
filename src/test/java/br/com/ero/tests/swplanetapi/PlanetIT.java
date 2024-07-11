b3f528ed36e649800cef6aa6b8a50578fbf8dbf9
Lucas Lima <lucaserolima@gmail.com>
2024-07-11 14:34:42 -0400

    Configuring o Banco MySQL: profile test

--- src/test/java/br/com/ero/tests/swplanetapi/PlanetIT.java
+++ src/test/java/br/com/ero/tests/swplanetapi/PlanetIT.java
@@ -2,11 +2,11 @@ package br.com.ero.tests.swplanetapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
 
-  @Test
-  public void contextLoads() {
-  }
+
 }
